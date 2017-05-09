1a33d33793b3a6d1a8685d9dd60dc00ca06b9887
luv-tungnt <nguyenthanhtung@luvina.net>
2017-05-09 15:56:49 +0700

    springboot gradle STS

---
+++ training-springboot-gradle/src/main/java/com/example/TrainingSpringbootGradleApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSpringbootGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingSpringbootGradleApplication.class, args);
+	}
+}
