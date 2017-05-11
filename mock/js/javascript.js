ab5af240b12eb85da432f26c1e9f2f8c4d329af6
luv-tungnt <nguyenthanhtung@luvina.net>
2017-05-11 17:34:44 +0700

    tạo nốt giao diện

---
+++ mock/js/javascript.js
@@ -0,0 +1,9 @@
+function showHide1(){
+	document.getElementById('selectCom').style.display='block';
+	document.getElementById('tblCom').style.display='none';
+}
+
+function showHide2(){
+	document.getElementById('tblCom').style.display='block';
+	document.getElementById('selectCom').style.display='none';
+}
